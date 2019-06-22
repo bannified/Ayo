74b625936a98ba4fe653aefdc06fde0ed8ce1023
bannified <freedomzelda_dc@hotmail.com>
2019-06-23 00:13:38 +0800

    Fix GetElements not being a const method

--- Ayo/src/Ayo/Renderer/Buffer.h
+++ Ayo/src/Ayo/Renderer/Buffer.h
@@ -79,13 +79,13 @@ namespace Ayo {
 			CalculateOffsetsAndStride();
 		};
 
-		inline const std::vector<BufferElement>& const GetElements() { return m_Elements; }
+		inline const std::vector<BufferElement>& GetElements() const { return m_Elements; }
 		inline uint32_t GetStride() const { return m_Stride; }
 
-		std::vector<BufferElement>::const_iterator begin() { return m_Elements.begin(); }
-		std::vector<BufferElement>::const_iterator end() { return m_Elements.end(); }
-		std::vector<BufferElement>::const_iterator cbegin() { return m_Elements.cbegin(); }
-		std::vector<BufferElement>::const_iterator cend() { return m_Elements.cend(); }
+		std::vector<BufferElement>::iterator begin() { return m_Elements.begin(); }
+		std::vector<BufferElement>::iterator end() { return m_Elements.end(); }
+		std::vector<BufferElement>::const_iterator begin() const { return m_Elements.cbegin(); }
+		std::vector<BufferElement>::const_iterator end() const { return m_Elements.cend(); }
 
 	private:
 		std::vector<BufferElement> m_Elements;
@@ -110,6 +110,9 @@ namespace Ayo {
 		virtual void Bind() const = 0;
 		virtual void Unbind() const = 0;
 
+		virtual void SetLayout(const BufferLayout& layout) = 0;
+		virtual const BufferLayout& GetLayout() const = 0;
+
 		static VertexBuffer* Create(float* vertices, uint32_t size);
 	};
 
@@ -120,9 +123,7 @@ namespace Ayo {
 		virtual void Bind() const = 0;
 		virtual void Unbind() const = 0;
 
-		virtual uint32_t GetCount() const = 0;
-		virtual void SetLayout(const BufferLayout& layout) = 0;
-		virtual const BufferLayout& GetLayout() const = 0;
+		virtual uint32_t GetCount() const = 0;		
 
 		static IndexBuffer* Create(uint32_t* indices, uint32_t count);
 	};
